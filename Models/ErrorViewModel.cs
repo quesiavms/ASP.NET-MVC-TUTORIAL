e81c3e12c1a420d1d5c61ab05f137d601900c651
Quesia Silva <quesiavirginia2806@gmail.com>
2025-02-11 10:58:26 -0300

    feat: first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
