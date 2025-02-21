6642b2201e833a5f0e3b6e39d1704f596b58f5ba
Quesia Silva <quesiavirginia2806@gmail.com>
2025-02-21 16:17:44 -0300

    feat: add multi-model view with CRUD

---
+++ Models/EmployeeUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCTutorial.Models
+{
+    public class EmployeeUserViewModel
+    {
+        public List<EmployeeViewModel> Employee { get; set; }
+        public List<SiteUserViewModel> SiteUser { get; set; }
+    }
+}
---
+++ Models/SiteUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCTutorial.Models
+{
+    public class SiteUserViewModel
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public Nullable<int> RoleID { get; set; }
+        public string RoleName { get; set; }
+    }
+}
